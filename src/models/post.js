86a5ae559d33eb1146f2969285f567ee85870d47
Wallace Frota <frotadev@gmail.com>
2024-02-14 15:27:02 -0300

    Criado model post

---
+++ src/models/post.js
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import database from "../config/db.js";
+
+const Posts = database.define("posts", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+export { Posts };
