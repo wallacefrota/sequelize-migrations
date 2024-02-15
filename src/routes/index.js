1fd80e6132c4fab446b1841670b2a9c4a0e463e0
Wallace Frota <frotadev@gmail.com>
2024-02-15 11:12:54 -0300

    Added routes from posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ import {Router} from 'express';
 const router = Router();
 
 import userRoute from './user.js';
+import postRoute from './posts.js'
 
 // routes user
 router.use('/users', userRoute);
+router.use('/posts', postRoute)
 
 export default router;
