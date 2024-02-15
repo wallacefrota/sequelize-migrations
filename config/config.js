9f1d8c2f3ae91ee15be26c021ce1d47f775a4000
Wallace Frota <frotadev@gmail.com>
2024-02-15 16:15:44 -0300

    config archive for migrations

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv-safe').config({allowEmptyValues: true});
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+  },
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+  },
+};
